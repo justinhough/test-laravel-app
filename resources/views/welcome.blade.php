b04171170e2f2fb451cb6b4738e1a52730ea3f37
Justin Hough <justinrayhough@gmail.com>
2021-09-10 20:16:10 +0000

    awesome code change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,10 @@
                     </svg>
                 </div>
 
+                <div>
+                    <h1 class="text-center">Awesome code change!</h1>
+                </div>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
